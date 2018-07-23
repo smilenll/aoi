8d1c921c28445fce5ed0de58c03a934d9c4dd830
Smilen <smilenlyubenov@gmail.com>
2018-07-23 13:16:54 +0300

    Update and delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -88,7 +88,13 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+       $request['detail'] = $request->description;
+       unset($request['description']);
+       $product->update($request->all());
+
+        return response([
+            'data'=> new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
@@ -99,6 +105,9 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+         $product->delete();
+        return response(
+            null
+        ,Response::HTTP_NO_CONTENT);
     }
 }
